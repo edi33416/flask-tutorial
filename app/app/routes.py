bcd2e616c61124482224ff9bc8eda3f9caae38f6
Eduard Staniloiu <edi33416@gmail.com>
2021-03-26 17:33:53 +0200

    Add login form with errors validation

--- app/app/routes.py
+++ app/app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
 from app.forms import LoginForm
 
 @app.route('/')
@@ -16,11 +16,11 @@ def index():
 
     return render_template("index.html", title="Test", user=user, posts=posts)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
+        flash("Login requested for user {}, remember_me={}".format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='Sign In', form=form)
+        return redirect(url_for("index"))
+    return render_template("login.html", title="Sign In", form=form)
