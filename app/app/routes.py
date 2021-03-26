9910753237d0d6d1c6112b3187762adf328d9b07
Eduard Staniloiu <edi33416@gmail.com>
2021-03-26 17:19:39 +0200

    Initial commit

---
+++ app/app/routes.py
@@ -0,0 +1,26 @@
+from app import app
+from flask import render_template
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route("/index")
+def index():
+    user = { "username": "Edi" }
+    posts = [
+            { "author": { "username": "Aaa"},
+               "body": "Bla bla"
+            },
+            { "author": { "username": "Baa"},
+               "body": "Bla bla bla"
+            }]
+
+    return render_template("index.html", title="Test", user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
