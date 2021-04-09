bdf069cc09bb36f5817a0cd59255e5adf51ea4e5
Eduard Staniloiu <edi33416@gmail.com>
2021-04-09 20:08:59 +0300

    Add user registration

--- app/app/routes.py
+++ app/app/routes.py
@@ -1,9 +1,10 @@
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 from flask import render_template, redirect, flash, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route("/index")
@@ -39,7 +40,7 @@ def login():
         # we must redirect to the initial page after a login
         next_page = request.args.get("next")
         # Ensure next_page is not empty or it's a local page, not a different site
-        if not next_page or url_parse(next_page).netlog != "":
+        if not next_page or url_parse(next_page).netloc != "":
             next_page = url_for("index")
         return redirect(next_page)
 
@@ -49,3 +50,20 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(username = form.username.data, email = form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congrats! You've logged in")
+        return redirect(url_for("login"))
+
+    return render_template("register.html", form = form)
