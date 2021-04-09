44c4f2d00992df14c4d2ff8b8fe302f7294a70fb
Eduard Staniloiu <edi33416@gmail.com>
2021-04-09 19:18:12 +0300

    Add user login functionality

--- app/app/models.py
+++ app/app/models.py
@@ -1,7 +1,11 @@
 from datetime import datetime
-from app import db
+from app import db, login
 
-class User(db.Model):
+from flask_login import UserMixin
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -12,8 +16,18 @@ class User(db.Model):
     post.author will retrieve the User that made the Post
     """
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- app/app/routes.py
+++ app/app/routes.py
@@ -1,9 +1,13 @@
 from app import app
-from flask import render_template, redirect, flash, url_for
 from app.forms import LoginForm
+from app.models import User
+
+from flask import render_template, redirect, flash, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
 
 @app.route('/')
 @app.route("/index")
+@login_required
 def index():
     user = { "username": "Edi" }
     posts = [
@@ -14,13 +18,34 @@ def index():
                "body": "Bla bla bla"
             }]
 
-    return render_template("index.html", title="Test", user=user, posts=posts)
+    return render_template("index.html", title="Test", posts=posts)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
     form = LoginForm()
     if form.validate_on_submit():
-        flash("Login requested for user {}, remember_me={}".format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for("index"))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalit username or password")
+            return redirect(url_for("login"))
+
+        # Login user in Flask-Login
+        login_user(user, remember=form.remember_me.data)
+
+        # If the user was redirected to the login page from a protected one
+        # we must redirect to the initial page after a login
+        next_page = request.args.get("next")
+        # Ensure next_page is not empty or it's a local page, not a different site
+        if not next_page or url_parse(next_page).netlog != "":
+            next_page = url_for("index")
+        return redirect(next_page)
+
     return render_template("login.html", title="Sign In", form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
