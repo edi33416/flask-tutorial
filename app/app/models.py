d039705afe33e2d2598b29e7f06b0ef6e4ad5065
Eduard Staniloiu <edi33416@gmail.com>
2021-04-13 14:48:35 +0300

    Create user profile page

--- app/app/models.py
+++ app/app/models.py
@@ -5,6 +5,8 @@ from flask_login import UserMixin
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from hashlib import md5
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -21,6 +23,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        # Get Gravatar
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+        return "https://www.gravatar.com/avatar/{}?d=identicon&s={}".format(digest, size)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
--- app/app/routes.py
+++ app/app/routes.py
@@ -67,3 +67,14 @@ def register():
         return redirect(url_for("login"))
 
     return render_template("register.html", form = form)
+
+# Create a user page
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {"author": user, "body": "Test post #1"},
+        {"author": user, "body": "Test post #2"}
+    ]
+    return render_template("user.html", user=user, posts=posts)
