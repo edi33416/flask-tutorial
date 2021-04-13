6cb242454b657224e42543ae56be12c17b1b5f06
Eduard Staniloiu <edi33416@gmail.com>
2021-04-13 16:14:33 +0300

    Send errors log info via email

---
+++ app/setup_email_envs.sh
@@ -0,0 +1,14 @@
+# Test local server with
+# python -m smtpd -n -c DebuggingServer localhost:8025
+
+export MAIL_SERVER=localhost
+export MAIL_PORT=8025
+#export MAIL_USE_TLS=0
+#export MAIL_USERNAME=<your-gmail-username>
+#export MAIL_PASSWORD=<your-gmail-password>
+
+#export MAIL_SERVER=smtp.googlemail.com
+#export MAIL_PORT=587
+#export MAIL_USE_TLS=1
+#export MAIL_USERNAME=<your-gmail-username>
+#export MAIL_PASSWORD=<your-gmail-password>
